e7e06aa09427feb0d93493514b4e7c6f90bd7906
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2017-04-06 15:01:07 -0700

    Simple service with API and pub sub

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	restful "github.com/emicklei/go-restful"
+	"github.com/micro/go-micro/broker"
+	"github.com/micro/go-micro/cmd"
+	_ "github.com/micro/go-plugins/broker/rabbitmq"
+	web "github.com/micro/go-web"
+)
+
+type Say struct{}
+
+func (s *Say) Anything(req *restful.Request, rsp *restful.Response) {
+	log.Print("Received Say.Anything API request")
+	rsp.WriteEntity(map[string]string{
+		"message": "Hi, this is the Greeter API",
+	})
+}
+
+func pub() {
+	cmd.Init()
+
+	if err := broker.Init(); err != nil {
+		log.Fatalf("Broker Init error: %v", err)
+	}
+	if err := broker.Connect(); err != nil {
+		log.Fatalf("Broker Connect error: %v", err)
+	}
+
+	tick := time.NewTicker(time.Second)
+	i := 0
+	for _ = range tick.C {
+		msg := &broker.Message{
+			Header: map[string]string{
+				"id": fmt.Sprintf("%d", i),
+			},
+			Body: []byte(fmt.Sprintf("%d: %s", i, time.Now().String())),
+		}
+		if err := broker.Publish("service.topic", msg); err != nil {
+			log.Printf("[pub] failed: %v", err)
+		} else {
+			fmt.Println("[pub] pubbed message:", string(msg.Body))
+		}
+		i++
+	}
+}
+
+func sub() {
+	cmd.Init()
+
+	if err := broker.Init(); err != nil {
+		log.Fatalf("Broker Init error: %v", err)
+	}
+	if err := broker.Connect(); err != nil {
+		log.Fatalf("Broker Connect error: %v", err)
+	}
+
+	_, err := broker.Subscribe("service.topic", func(p broker.Publication) error {
+		fmt.Println("[sub] received message:", string(p.Message().Body), "header", p.Message().Header)
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	cmd.Init()
+
+	go pub()
+	go sub()
+
+	service := web.NewService(
+		web.Name("api.service"),
+		web.Address("[::]:8080"),
+		web.Advertise("[::]:8080"),
+	)
+
+	service.Init()
+
+	// Create RESTful handler
+	say := new(Say)
+	ws := new(restful.WebService)
+	wc := restful.NewContainer()
+	ws.Consumes(restful.MIME_XML, restful.MIME_JSON)
+	ws.Produces(restful.MIME_JSON, restful.MIME_XML)
+	ws.Path("/service")
+	ws.Route(ws.GET("/").To(say.Anything))
+	wc.Add(ws)
+
+	// Register Handler
+	service.Handle("/", wc)
+
+	// Run server
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
